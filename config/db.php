66ddecae39aa24183d73e3b951022bf30dc5660b
Jhonny Roemro <jromero@zugartek.com>
2016-11-04 19:00:49 -0500

    Resolviendo issues #355

--- config/db.php
+++ config/db.php
@@ -1,20 +1,10 @@
 <?php
-//LOCAL
-/*
+
 return [
+//            Sansbox
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tiver',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
-*/
-//DEV
-return [
-		'class' => 'yii\db\Connection',
-		//'dsn' => 'mysql:host=tiver.c0hs9r9wuhtq.us-west-2.rds.amazonaws.com;dbname=tiver',
-		'dsn' => 'mysql:host=tiver.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;dbname=tiver',
-		'username' => 'root',
-		'password' => 'tiverroot',
-		'charset' => 'utf8',
+    'dsn' => 'tvrsandbox.cqjbs2tah4ps.us-east-1.rds.amazonaws.com',
+    'username' => 'tvrsandbox',
+    'password' => 'tvrsandbox',
+    'charset' => 'tvrsandbox',
 ];
