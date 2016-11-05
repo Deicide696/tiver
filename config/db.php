626ee4ff46529a9f504aa0db7c1b1fa915c8abb6
Cristian Pinilla <cristian.camilo6@gmail.com>
2016-11-04 20:06:06 -0500

    No hay cambios

--- config/db.php
+++ config/db.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-//            Sansbox
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=tvrsandbox.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;port=3306;dbname=tvrsandbox;',
     // 'dsn' => 'mysql:host=devcotizadorco.c9rav09qhooy.us-east-1.rds.amazonaws.com;port=3306;dbname=devcotizadorco;',
