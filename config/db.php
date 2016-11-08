778642481763fa85c4ba77b127b7aca9569e7626
Jhonny Roemro <jromero@zugartek.com>
2016-11-08 16:16:39 -0500

    Corrigiendo error en conexion en BD

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=tvrsandbox.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;port=3306;dbname=tvrsandbox;',
+    'dsn' => 'mysql:host=tvrsandbox.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;dbname=tvrsandbox;',
     // 'dsn' => 'mysql:host=devcotizadorco.c9rav09qhooy.us-east-1.rds.amazonaws.com;port=3306;dbname=devcotizadorco;',
     'username' => 'tvrsandbox',
     'password' => 'tvrsandbox',
