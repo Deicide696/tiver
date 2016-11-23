bd6ae154926b08ce4c360a9017563d3adc1a7ae5
Cristian Pinilla <cristian.camilo6@gmail.com>
2016-11-22 19:00:14 -0500

    Cambio a la base de datos Sandbox

--- config/db.php
+++ config/db.php
@@ -1,18 +1,18 @@
 <?php
 
-//return [
-//    'class'     => 'yii\db\Connection',
-//    'dsn'       => 'mysql:host=sandboxtiver.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;port=3306;dbname=tiver;',
-//    'username'  => 'root',
-//    'password'  => 'tiverroot',
-//    'charset'   => 'utf8',
-//];
-
 return [
-		'class' => 'yii\db\Connection',
-		//'dsn' => 'mysql:host=tiver.c0hs9r9wuhtq.us-west-2.rds.amazonaws.com;dbname=tiver',
-		'dsn' => 'mysql:host=tiver.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;dbname=tiver',
-		'username' => 'root',
-		'password' => 'tiverroot',
-		'charset' => 'utf8',
+   'class'     => 'yii\db\Connection',
+   'dsn'       => 'mysql:host=sandboxtiver.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;port=3306;dbname=tiver;',
+   'username'  => 'root',
+   'password'  => 'tiverroot',
+   'charset'   => 'utf8',
 ];
+
+// return [
+// 		'class' => 'yii\db\Connection',
+// 		//'dsn' => 'mysql:host=tiver.c0hs9r9wuhtq.us-west-2.rds.amazonaws.com;dbname=tiver',
+// 		'dsn' => 'mysql:host=tiver.cqjbs2tah4ps.us-east-1.rds.amazonaws.com;dbname=tiver',
+// 		'username' => 'root',
+// 		'password' => 'tiverroot',
+// 		'charset' => 'utf8',
+// ];
